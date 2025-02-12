850e0daeb94a15ee691cd6f50650fb715f46ffe9
webdevmahabub <mahbubbd926@gmail.com>
2025-02-12 23:09:15 +0600

    Get Data From Database in Course Details Page Part 1

--- app/(main)/courses/[id]/page.jsx
+++ app/(main)/courses/[id]/page.jsx
@@ -3,51 +3,11 @@ import CourseDetailsIntro from "./_components/CourseDetailsIntro";
 import CourseDetails from "./_components/CourseDetails";
 import Testimonials from "./_components/Testimonials";
 import RelatedCourses from "./_components/RelatedCourses";
-
-// const courses = [
-//   {
-//     id: 1,
-//     title: "Design",
-//     thumbnail: "/assets/images/categories/design.jpg",
-//   },
-
-//   {
-//     id: 3,
-//     title: "Development",
-//     thumbnail: "/assets/images/categories/development.jpg",
-//   },
-//   {
-//     id: 4,
-//     title: "Marketing",
-//     thumbnail: "/assets/images/categories/marketing.jpg",
-//   },
-//   {
-//     id: 5,
-//     title: "IT & Software",
-//     thumbnail: "/assets/images/categories/it_software.jpg",
-//   },
-//   {
-//     id: 6,
-//     title: "Personal Development",
-//     thumbnail: "/assets/images/categories/personal_development.jpg",
-//   },
-//   {
-//     id: 7,
-//     title: "Business",
-//     thumbnail: "/assets/images/categories/business.jpg",
-//   },
-//   {
-//     id: 8,
-//     title: "Photography",
-//     thumbnail: "/assets/images/categories/photography.jpg",
-//   },
-//   {
-//     id: 9,
-//     title: "Music",
-//     thumbnail: "/assets/images/categories/music.jpg",
-//   },
-// ];
-const SingleCoursePage = () => {
+import { getCourseDetails } from "@/queries/courses";
+ 
+const SingleCoursePage = async ({ params: {id} }) => {
+    const course = await getCourseDetails(id);
+    console.log(course);
   return (
     <>
       <CourseDetailsIntro/>
--- queries/courses.js
+++ queries/courses.js
@@ -3,7 +3,7 @@ import { Course } from "@/model/course-model";
 import { Module } from "@/model/module.model";
 import { Testimonial } from "@/model/testimonial-model";
 import { User } from "@/model/user-model";
-import { replaceMongoIdInArray } from "@/lib/convertData";
+import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData";
 export async function getCourseList() {
     const courses = await Course.find({}).select(["title","subtitle","thumbnail","modules","price","category","instructor"]).populate({
         path: "category",
@@ -19,4 +19,25 @@ export async function getCourseList() {
         model: Module
     }).lean();
     return replaceMongoIdInArray(courses);
+}  
+export async function getCourseDetails(id) {
+    const course = await Course.findById(id)
+    .populate({
+        path: "category",
+        model: Category
+    }).populate({
+        path: "instructor",
+        model: User
+    }).populate({
+        path: "testimonials",
+        model: Testimonial,
+        populate: {
+            path: "user",
+            model: User
+        }
+    }).populate({
+        path: "modules",
+        model: Module
+    }).lean();
+    return replaceMongoIdInObject(course);
 }  
