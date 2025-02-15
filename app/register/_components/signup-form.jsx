4b1a1c894e8b7a03a40de38900a510bda46914cd
webdevmahabub <mahbubbd926@gmail.com>
2025-02-15 19:34:50 +0600

    Register and Become an Instructor, Login Part 2

--- app/register/_components/signup-form.jsx
+++ app/register/_components/signup-form.jsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,7 +10,37 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-export function SignupForm() {
+import { useRouter } from "next/navigation";
+export function SignupForm({role}) {
+  const router = useRouter();
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const formData = new FormData(event.currentTarget);
+      const firstName = formData.get("first-name");
+      const lastName = formData.get("last-name");
+      const email = formData.get("email");
+      const password = formData.get("password");
+      const userRole = ((role === "student" ) || (role === "instructor")) ? role : "student";
+      const response = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          password,
+          userRole 
+        })
+      });
+      response.status === 201 && router.push("/login")
+      
+    } catch (e) {
+      console.log(e.message);
+    } 
+  }
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
@@ -25,7 +56,7 @@ export function SignupForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+      <form onSubmit={onSubmit} >
         <div className="grid gap-4">
           <div className="grid grid-cols-2 gap-4">
             <div className="grid gap-2">
@@ -34,13 +65,14 @@ export function SignupForm() {
             </div>
             <div className="grid gap-2">
               <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" name="last-name" placeholder="Robinson" required />
+              <Input id="last-name" name="last-name"  placeholder="Robinson" required />
             </div>
           </div>
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
               id="email"
+              name="email" 
               type="email"
               placeholder="m@example.com"
               required
@@ -48,11 +80,11 @@ export function SignupForm() {
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
+            <Input id="password" name="password" type="password" />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="confirmPassword">Confirm Password</Label>
-            <Input id="confirmPassword" type="password" />
+            <Input id="confirmPassword" name="confirmPassword" type="password" />
           </div>
           <Button type="submit" className="w-full">
             Create an account
