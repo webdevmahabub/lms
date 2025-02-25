cf3f4e1258f55796a90cd68e28bbbe3e3f6a4141
webdevmahabub <mahbubbd926@gmail.com>
2025-02-25 19:35:07 +0600

    Reset Password and Account Update Part 3

---
+++ app/(main)/account/component/change-password.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import Link from "next/link";
+
+const ChangePassword = () => {
+    return (
+        <div>
+<h5 className="text-lg font-semibold mb-4">
+    Change password :
+</h5>
+<form>
+    <div className="grid grid-cols-1 gap-5">
+        <div>
+            <Label className="mb-2 block">Old password :</Label>
+            <Input
+                type="password"
+                placeholder="Old password"
+                required=""
+            />
+        </div>
+        <div>
+            <Label className="mb-2 block">New password :</Label>
+            <Input
+                type="password"
+                placeholder="New password"
+                required=""
+            />
+        </div>
+        <div>
+            <Label className="mb-2 block">
+                Re-type New password :
+            </Label>
+            <Input
+                type="password"
+                placeholder="Re-type New password"
+                required=""
+            />
+        </div>
+    </div>
+    {/*end grid*/}
+    <Button className="mt-5" type="submit">
+        Save password
+    </Button>
+</form>
+</div>
+    );
+};
+
+export default ChangePassword;
