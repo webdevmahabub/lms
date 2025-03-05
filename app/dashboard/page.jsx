2c044f5e8f1227880655f5b0d67666d44ebba55c
webdevmahabub <mahbubbd926@gmail.com>
2025-03-05 23:26:56 +0600

    Dashboard - Features, Analytics, Profile, Logout Part 4

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,27 @@
+import { auth } from "@/auth";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatPrice } from "@/lib/formatPrice";
+import { getCourseDetailsByInstructor } from "@/queries/courses";
+import { getUserByEmail } from "@/queries/users";
+import { redirect } from "next/navigation";
 formatPrice;
 
+
 const DashboardPage = async () => {
+
+  const session = await auth();
+  if (!session?.user) redirect("/login");
+  
+  const instructor = await getUserByEmail(session.user.email);
+  //console.log(instructor);
+  if (instructor?.role !== "instructor") redirect("/login");
+
+  const courseStatus = await getCourseDetailsByInstructor(instructor?.id);
+  console.log(courseStatus);
+
   return (
     <div className="p-6">
+      <p className="pb-4 text-gray-800"> Login as : <strong>{courseStatus?.fullInsName}</strong> </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
         {/* total courses */}
         <Card>
@@ -12,7 +29,7 @@ const DashboardPage = async () => {
             <CardTitle className="text-sm font-medium">Total Courses</CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">15</div>
+          <div className="text-2xl font-bold">{courseStatus?.courses}</div>
           </CardContent>
         </Card>
         {/* total enrollments */}
@@ -23,7 +40,7 @@ const DashboardPage = async () => {
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">1000</div>
+          <div className="text-2xl font-bold">{courseStatus?.enrollments}</div>
           </CardContent>
         </Card>
         {/* total revinue */}
@@ -32,7 +49,7 @@ const DashboardPage = async () => {
             <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">{formatPrice(12000)}</div>
+          <div className="text-2xl font-bold">{formatPrice(courseStatus?.revenue)}</div>
           </CardContent>
         </Card>
       </div>
