223f9b2022fd71c94fe12ff018676320991971ac
webdevmahabub <mahbubbd926@gmail.com>
2025-03-06 20:00:34 +0600

    Dashboard - Features, Analytics, Profile, Logout Part 5

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -15,10 +15,21 @@ const MainNav = ({items,children}) => {
     const {data:session} = useSession();
     const [showMobileMenu, setShowMobileMenu] = useState(false);
     const [loginSession, setLoginSession] = useState(null);
+    const [loggedInUser, setLoggedInUser] = useState(null);
 
-    useEffect(() => {
-        console.log("Test information");
+    useEffect(() => { 
         setLoginSession(session);
+        async function fetchMe() {
+            try {
+                const response = await fetch("/api/me");
+                const data = await response.json();
+                console.log(data);
+                setLoggedInUser(data);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchMe();
     },[session]);
 
     return (
