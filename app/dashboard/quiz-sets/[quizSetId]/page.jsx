0f57214502d1c5b9ab002828b2b980c81a62e932
webdevmahabub <mahbubbd926@gmail.com>
2025-03-20 20:04:12 +0600

    Create Course Quiz Option Part 5

--- app/dashboard/quiz-sets/[quizSetId]/page.jsx
+++ app/dashboard/quiz-sets/[quizSetId]/page.jsx
@@ -1,4 +1,3 @@
-"use client";
 import AlertBanner from "@/components/alert-banner";
 import { IconBadge } from "@/components/icon-badge";
 import { LayoutDashboard } from "lucide-react";
@@ -6,7 +5,6 @@ import { QuizSetAction } from "./_components/quiz-set-action";
 import { TitleForm } from "./_components/title-form";
 import { AddQuizForm } from "./_components/add-quiz-form";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
 import { Pencil } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Delete } from "lucide-react";
@@ -15,58 +13,25 @@ import { CircleCheck } from "lucide-react";
 import { Circle } from "lucide-react";
 import { getQuizSetById } from "@/queries/quizzes";
 
-const initialQuizes = [
-  {
-    id: 1,
-    title: "What is HTML ?",
-    options: [
-      {
-        label: "A programming language",
-        isTrue: false,
-      },
-      {
-        label: "A markup language",
-        isTrue: true,
-      },
-      {
-        label: "A famous book",
-        isTrue: false,
-      },
-      {
-        label: "A famous tv show",
-        isTrue: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "What is Javascript ?",
-    options: [
-      {
-        label: "A programming language",
-        isTrue: true,
-      },
-      {
-        label: "A markup language",
-        isTrue: false,
-      },
-      {
-        label: "A famous book",
-        isTrue: false,
-      },
-      {
-        label: "A famous tv show",
-        isTrue: false,
-      },
-    ],
-  },
-];
-
 const EditQuizSet = async ({ params: {quizSetId} }) => {
 
   const quizSet = await getQuizSetById(quizSetId);
-  console.log(quizSet);
-  const [quizes, setQuizes] = useState(initialQuizes);
+
+  const quizzes = quizSet.quizIds.map(quiz => {
+    return {
+      id: quiz._id.toString(),
+      title: quiz.title,
+      options: quiz.options.map(option => {
+        return {
+          label: option.text,
+          isTrue: option.is_correct,
+        }
+      })
+    }
+  })
+ // console.log(quizzes);
+
+  // const [quizes, setQuizes] = useState(initialQuizes);
   
   return (
     <>
@@ -88,7 +53,7 @@ const EditQuizSet = async ({ params: {quizSetId} }) => {
               className="rounded mb-6"
             />
             <div className="space-y-6">
-              {quizes.map((quiz) => {
+            {quizzes.map((quiz) => {
                 return (
                   <div
                     key={quiz.id}
@@ -147,7 +112,7 @@ const EditQuizSet = async ({ params: {quizSetId} }) => {
             </div>
 
             <div className="max-w-[800px]">
-              <AddQuizForm setQuizes={setQuizes} />
+              <AddQuizForm />
             </div>
           </div>
         </div>
